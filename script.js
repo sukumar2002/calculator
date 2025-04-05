ace0d80168c7ff0dcbefd34d8b43539d3a61bb95
sukumar2002 <127292893+sukumar2002@users.noreply.github.com>
2025-04-05 23:30:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 let dis = document.getElementById('display');
 
-
 function appendToDisplay(value) {
     display.value += value;
 }
-
-
 function clearDisplay() {
     display.value = '';
 }
-
 
 function calculateResult() {
     try {
